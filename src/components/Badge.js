0f5e8e453081c3b10970007b50a30f083555e389
Rafael Flores <fita500@gmail.com>
2020-08-09 18:59:10 -0600

    Add images and styles

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,27 +1,33 @@
 import React from "react";
+import confLogo from "../images/badge-header.svg";
+import "./styles/Badge.css";
 
 class Badge extends React.Component {
   render() {
     return (
-      <div>
-        <div>
-          <img src="" alt="Logo de la conferencia" />
+      <div className="Badge">
+        <div className="Badge__header">
+          <img src={confLogo} alt="Logo de la conferencia" />
         </div>
 
-        <div>
-          <img src="" alt="Avatar" />
+        <div className="Badge__section-name">
+          <img
+            src="https://www.gravatar.com/avatar?id=identicon"
+            alt="Avatar"
+            className="Badge__avatar"
+          />
           <h1>
             Rafael <br />
             Flores
           </h1>
         </div>
 
-        <div>
-          <p>Desarrollador web</p>
-          <p>@elfitaflores</p>
+        <div className="Badge__section-info">
+          <h3>Desarrollador web</h3>
+          <div>@elfitaflores</div>
         </div>
 
-        <div>#platziconf</div>
+        <div className="Badge__footer">#platziconf</div>
       </div>
     );
   }
