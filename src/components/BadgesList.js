7638d061bb7b8c05e4dd9d315b56b68d6f94ab89
Rafael Flores <fita500@gmail.com>
2020-08-16 18:03:54 -0600

    Get badges from API

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,8 +1,19 @@
 import React from "react";
 import "./styles/BadgesList.css";
+import { Link } from "react-router-dom";
 
 class BadgesList extends React.Component {
   render() {
+    if (this.props.badges.length === 0) {
+      return (
+        <div>
+          <h3>No encontramos ningún badge</h3>
+          <Link to="/badge/new" className="btn btn-primary">
+            Create new
+          </Link>
+        </div>
+      );
+    }
     return (
       <ul className="list-unstyled">
         {this.props.badges.map((badge) => {
